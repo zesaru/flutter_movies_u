a48946d0160da1c8463d0ffca3e495510c68e3ed
Cesar Murillo <namiayumis@gmail.com>
2019-08-26 12:21:23 +0900

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:movies/pages/home_page.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Películas',
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
