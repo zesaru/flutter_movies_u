ae5172b617a7a5bd9fdcb30e798b9dc3c0904413
Cesar Murillo <namiayumis@gmail.com>
2019-08-26 15:53:38 +0900

    CardSwipper

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('HOla mundo'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas actuales'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swipperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swipperTarjetas() {
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      padding: EdgeInsets.only(top: 20.0),
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 250.0,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
       ),
     );
   }
