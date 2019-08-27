e2bdb79410e90549de6e5e3744d7e2691abf40ce
Cesar Murillo <namiayumis@gmail.com>
2019-08-27 17:49:15 +0900

    widget horizontal personalizado

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies/providers/peliculas_provider.dart';
 import 'package:movies/widgets/card_swiper_widget.dart';
+import 'package:movies/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final peliculasProvider = new PeliculasProvider();
@@ -53,23 +54,20 @@ class HomePage extends StatelessWidget {
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(
-            'Populares',
-            style: Theme.of(context).textTheme.subhead,
-          ),
+          Container(
+              padding: EdgeInsets.only(left: 22.0),
+              child: Text('Populares',
+                  style: Theme.of(context).textTheme.subhead)),
+          SizedBox(height: 5.0),
           FutureBuilder(
             future: peliculasProvider.getPopulares(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
-                return CardSwiper(peliculas: snapshot.data);
+                return MovieHorizontal(peliculas: snapshot.data);
               } else {
-                return Container(
-                  height: 100.0,
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                );
+                return Center(child: CircularProgressIndicator());
               }
             },
           ),
