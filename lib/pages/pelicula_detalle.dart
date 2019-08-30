99b43a8b6b59a22796f7e88a00733b0495943209
Cesar Murillo <namiayumis@gmail.com>
2019-08-30 15:44:47 +0900

    pantalla detalle 2

--- lib/pages/pelicula_detalle.dart
+++ lib/pages/pelicula_detalle.dart
@@ -6,9 +6,23 @@ class PeliculaDetalle extends StatelessWidget {
   Widget build(BuildContext context) {
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-        body: CustomScrollView(
-      slivers: <Widget>[_crearAppBar(pelicula)],
-    ));
+      body: CustomScrollView(
+        slivers: <Widget>[
+          _crearAppBar(pelicula),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10.0,
+              ),
+              _posterTitulo(context, pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+            ]),
+          )
+        ],
+      ),
+    );
   }
 
   Widget _crearAppBar(Pelicula pelicula) {
@@ -31,6 +45,57 @@ class PeliculaDetalle extends StatelessWidget {
           image: NetworkImage(pelicula.getBackgroundImg()),
           placeholder: AssetImage('assets/img/loading.gif'),
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage(pelicula.getPosterImg()),
+                height: 150.0,
+              ),
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(pelicula.title,
+                    style: Theme.of(context).textTheme.title,
+                    overflow: TextOverflow.ellipsis),
+                Text(pelicula.originalTitle,
+                    style: Theme.of(context).textTheme.subhead,
+                    overflow: TextOverflow.ellipsis),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(pelicula.voteAverage.toString(),
+                        style: Theme.of(context).textTheme.subhead)
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
--- lib/widgets/movie_horizontal.dart
+++ lib/widgets/movie_horizontal.dart
@@ -67,31 +67,4 @@ class MovieHorizontal extends StatelessWidget {
       },
     );
   }
-
-  // no se esta usando tarjetas
-  List<Widget> _tarjetas(BuildContext context) {
-    return peliculas.map((pelicula) {
-      return Container(
-        margin: EdgeInsets.only(right: 15.0),
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                image: NetworkImage(pelicula.getPosterImg()),
-                placeholder: AssetImage('assets/img/no-image.jpg'),
-                fit: BoxFit.cover,
-                height: 160.0,
-              ),
-            ),
-            Text(
-              pelicula.title,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  }
 }
