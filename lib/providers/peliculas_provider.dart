2fa496f9b1e2b8dc05b7cd98adb25839d350c5d2
Cesar Murillo <namiayumis@gmail.com>
2019-08-30 17:18:07 +0900

    get Cast -> peliculas provider

--- lib/providers/peliculas_provider.dart
+++ lib/providers/peliculas_provider.dart
@@ -1,9 +1,9 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:core';
 import 'package:http/http.dart' as http;
-
+import 'package:movies/models/actores_model.dart';
 import 'package:movies/models/pelicula_model.dart';
+
+import 'dart:convert';
+import 'dart:async';
 
 class PeliculasProvider {
   String _apikey = '4d22b6beed3cf5acfff824bd5e57db60';
@@ -70,5 +70,17 @@ class PeliculasProvider {
 
     _cargando = false;
     return resp;
+  }
+
+  Future<List<Actor>> getCast(String peliId) async {
+    final url = Uri.https(_url, '3/movie/$peliId/credits',
+        {'api_key': _apikey, 'language': _language});
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+
+    return cast.actores;
   }
 }
