963748ab37f2c736eb21db81338f5b24b1956f16
Cesar Murillo <namiayumis@gmail.com>
2019-09-01 21:08:03 +0900

    search delegate

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movies/providers/peliculas_provider.dart';
+import 'package:movies/search/search_delegate.dart';
 import 'package:movies/widgets/card_swiper_widget.dart';
 import 'package:movies/widgets/movie_horizontal.dart';
 
@@ -18,7 +19,13 @@ class HomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: DataSearch(),
+                // query: 'Hola'
+              );
+            },
           )
         ],
       ),
