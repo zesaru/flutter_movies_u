044673e6bfecaffba74d3af7f8a1ed273043ce85
Cesar Murillo <namiayumis@gmail.com>
2019-09-01 21:38:15 +0900

    BuildActions BuildLeading

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -4,24 +4,39 @@ class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro AppBar
-    return null;
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     // Icono a la izquierda del AppBar
-    return null;
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que vamos a mostrar
-    return null;
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return null;
+    return Container();
   }
 }
